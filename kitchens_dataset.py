69a316ef8ad34df477adc609d555c64ac2b38597
Rhodri Guerrier <rhodriguerrier98@gmail.com>
2022-10-12 17:51:19 +0100

    Home work 12/10/2022 - return only rgb or flow for separate training

--- kitchens_dataset.py
+++ kitchens_dataset.py
@@ -6,30 +6,34 @@ import numpy as np
 from PIL import Image
 
 
-class RgbFlowKitchensDataset(Dataset):
-    def __init__(self, labels_path):
+class EpicKitchensDataset(Dataset):
+    def __init__(self, labels_path, is_flow=False):
         labels_df = load_pickle_data(labels_path)
         self.labels = []
+        self.is_flow = is_flow
         self.rgb_input_names = []
         self.flow_input_names = []
         for index, row in labels_df.iterrows():
-            rgb_seg_img_names, flow_seg_img_names = sample_train_segment(
+            seg_img_names = sample_train_segment(
                 16,
                 row["start_frame"],
                 row["stop_frame"],
                 row["participant_id"],
-                row["video_id"]
+                row["video_id"],
+				is_flow=is_flow
             )
             self.labels.append(row["verb_class"])
-            self.rgb_input_names.append(rgb_seg_img_names)
-            self.flow_input_names.append(flow_seg_img_names)
+            self.input_names.append(seg_img_names)
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, index):
         print(index)
-        return self.labels[index], load_rgb_frames(self.rgb_input_names[index]), load_flow_frames(self.flow_input_names[index])
+		if self.is_flow:
+            return self.labels[index], load_flow_frames(self.input_names[index])
+        else:
+            return self.labels[index], load_rgb_frames(self.rgb_input_names[index])
 
 
 def load_pickle_data(file_name):
@@ -61,11 +65,10 @@ def load_rgb_frames(rgb_filenames):
     return (((frames_reshape / 255) * 2) - 1)
 
 
-def sample_train_segment(temporal_window, start_frame, end_frame, domain_num, part_id):
+def sample_train_segment(temporal_window, start_frame, end_frame, domain_num, part_id, is_flow):
     half_frame = int(temporal_window/2)
     step = 2
-    rgb_seg_img_names = []
-    flow_seg_img_names = []
+    seg_img_names = []
     segment_start = int(start_frame) + (step*half_frame)
     segment_end = int(end_frame) + 1 - (step*half_frame)
     # Write a comment to explain this weirdness
@@ -76,18 +79,20 @@ def sample_train_segment(temporal_window, start_frame, end_frame, domain_num, pa
         segment_start <= half_frame*step+2
     centre_frame = randint(segment_start, segment_end)
     for i in range(centre_frame-(step*half_frame), centre_frame+(step*half_frame), step):
-        rgb_seg_img_names.append(f"./epic_kitchens_data/rgb/{domain_num}/{part_id}/frame_{str(i).zfill(10)}.jpg")
-        flow_seg_img_names.append([
-            f"./epic_kitchens_data/flow/{domain_num}/{part_id}/u/frame_{str(int(i/2)).zfill(10)}.jpg",
-            f"./epic_kitchens_data/flow/{domain_num}/{part_id}/v/frame_{str(int(i/2)).zfill(10)}.jpg"
-        ])
-    return rgb_seg_img_names, flow_seg_img_names
+        if is_flow:
+            seg_img_names.append([
+                f"./epic_kitchens_data/flow/{domain_num}/{part_id}/u/frame_{str(int(i/2)).zfill(10)}.jpg",
+                f"./epic_kitchens_data/flow/{domain_num}/{part_id}/v/frame_{str(int(i/2)).zfill(10)}.jpg"
+            ])
+        else:
+            seg_img_names.append(f"./epic_kitchens_data/rgb/{domain_num}/{part_id}/frame_{str(i).zfill(10)}.jpg"
+    return seg_img_names
 
 
 if __name__ == "__main__":
-    train_dataset = RgbFlowKitchensDataset(labels_path="../MM-SADA-code/Annotations/D2_train.pkl")
+    train_dataset = EpicKitchensDataset(labels_path="../MM-SADA-code/Annotations/D2_train.pkl", False)
     train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=False)
-    for (labels, rgb_inputs, flow_inputs) in train_dataloader:
+    for (labels, rgb_inputs) in train_dataloader:
         print(labels)
         print(labels.shape)
         break
